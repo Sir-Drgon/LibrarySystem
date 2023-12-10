e5f8cbda3328d1377a00e1865304f078b2a9fc8e
Sir-Drgon <andremondragon6722@gmail.com>
2023-12-10 02:45:43 -0500

    afa

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("hello World");
         for (int i = 0; i < 10; i++){
             System.out.println(i);
-            System.out.println("hello");
+            System.out.println("helo");
         }
     }
 }
